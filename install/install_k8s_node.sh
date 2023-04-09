f2f49c86cb8bb91a7011bc0b4df641e58ff5e2c5
zhiqli <31506212+li1079301645@users.noreply.github.com>
2023-04-09 12:40:35 +0800

    fix

--- install/install_k8s_node.sh
+++ install/install_k8s_node.sh
@@ -5,3 +5,16 @@ sh ./components/install_containerd.sh
 
 sh ./components/install_kube.sh
 
+echo '>>> disable swap'
+sudo swapoff -a
+sudo sed -i '/ swap / s/^(.*)$/#1/g' /etc/fstab
+
+sudo modprobe br_netfilter
+sudo echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+
+sudo crictl pull registry.aliyuncs.com/google_containers/pause:3.6
+
+sudo ctr -n k8s.io image tag --force registry.aliyuncs.com/google_containers/pause:3.6 registry.k8s.io/pause:3.6
+
+
